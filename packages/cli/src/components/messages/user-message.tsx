09e0d3f4583a257eef80bcc7ba648a33c1b93269
Mushroom Singh <mushroomsingh@Mushrooms-MacBook-Air.local>
2026-07-20 00:49:00 +0530

    03: routing-screen-layout

---
+++ packages/cli/src/components/messages/user-message.tsx
@@ -0,0 +1,29 @@
+import { useTheme } from "../../providers/theme";
+
+type Props = {
+    message: string
+}
+
+export function UserMessage({message}: Props){
+    const {colors} = useTheme()
+
+    return(
+        <box width="100%" alignItems="center">
+            <box
+            border={["left"]}
+            borderColor={colors.error}
+            width="100%"
+            >
+                <box
+                justifyContent="center"
+                paddingX={2}
+                paddingY={1}
+                backgroundColor={colors.primary}
+                width="100%"
+                >
+                    <text>{message}</text>
+                </box>
+            </box>
+        </box>
+    )
+}
