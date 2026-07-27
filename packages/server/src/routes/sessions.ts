be3871cee555419534b92568def8d5f11043cc9b
Mushroom Singh <mushroomsingh@Mushrooms-MacBook-Air.local>
2026-07-27 19:08:25 +0530

    fixed changes

--- packages/server/src/routes/sessions.ts
+++ packages/server/src/routes/sessions.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { zValidator } from "@hono/zod-validator";
 import {z} from "zod";
-import {db} from "@mushroomcode/database"
+import {db} from "@mushroomcode/database/client"
 import {Role, Mode, MessageStatus} from "@mushroomcode/database/enums"
 import { findSupportedChatModel } from "@mushroomcode/shared";
 
